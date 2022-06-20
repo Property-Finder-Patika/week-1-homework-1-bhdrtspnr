725784c4ec9f2e4f0d3c9bc3cd35573cb14137c9
bhdrtspnr <bahadir.taspinar01@gmail.com>
2022-06-20 23:19:13 +0300

    osArgs usage with age update

--- osArgs_example.go
+++ osArgs_example.go
@@ -7,12 +7,14 @@ import (
 
 func main() {
 	name := os.Args[1] //0th arg is the go file name, so 1st arg is the name
-	var greeting string = createGreet(name)
+	age := os.Args[2]
+
+	var greeting string = createGreet(name, age)
 	fmt.Printf("%s", greeting)
 
 }
 
-func createGreet(name string) string {
-	greeting := "Hello " + name + " :) "
+func createGreet(name string, age string) string {
+	greeting := "Hello " + name + " :) " + "You are " + age + " years old"
 	return greeting
 }
