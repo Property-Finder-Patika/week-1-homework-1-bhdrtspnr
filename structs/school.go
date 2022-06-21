254442eedfd31cbdb07a3d74446ce5bedebfd687
bhdrtspnr <bahadir.taspinar01@gmail.com>
2022-06-21 14:26:11 +0300

    adding person struct to school struct's list of students

--- structs/school.go
+++ structs/school.go
@@ -17,7 +17,7 @@ func (s School) toString() { //not returning string as it was in the person this
 	fmt.Println("Name: " + s.Name)
 	fmt.Println("Cİty:" + s.City)
 	fmt.Println("Student count: " + strconv.Itoa(len(s.Students))) //count of the students is the length of the students slice for each school
-
+	fmt.Println("Students:")
 	for i := 0; i < len(s.Students); i++ { //iterate over the students to print all of them
 		fmt.Println(s.Students[i].toString())
 	}
@@ -28,22 +28,25 @@ func addSchool(name string, city string) { //adds a school to the slice schools
 	schools = append(schools, newSchool)
 }
 
-func addStudent() { //not finished yet
-	/*
-		fmt.Println("Enter the name of the school to add the student to")
-		var schoolName string
-		fmt.Scanln(&schoolName)
-		fmt.Println("Enter the name of the student")
-		var studentName string
-		fmt.Scanln(&studentName)
+func addStudent() {
+	fmt.Print("Enter the name of the school: ")
+	var schoolName string
+	fmt.Scanln(&schoolName)
+	for i := 0; i < len(schools); i++ {
+		if schools[i].Name == schoolName {
+			fmt.Print("School exists!")
+			fmt.Print("Enter the name of the student to be added: ")
+			var studentName string
+			fmt.Scanln(&studentName)
+			for i := 0; i < len(persons); i++ {
+				if persons[i].name == studentName {
+					schools[i].Students = append(schools[i].Students, persons[i])
+					fmt.Print("Student added!")
+				}
 
-		for i := 0; i < len(schools); i++ { //iterate over the schools to find the school with the given name
-			if schools[i].Name == schoolName {
-				schools[i].Students = append(schools[i].Students, persons{Name: studentName}) //add the student to the school's students slice
 			}
 		}
-
-	*/
+	}
 }
 
 func listSchools() { //iterate over list to print all schools
